eff45d8ee5d06ac9a23b134c8ba0c02d63aac35a
complected <105940396+complected@users.noreply.github.com>
2022-05-28 12:25:51 -0700

    Implement scry and sign with ethers.js (#2)
    * use ethersjs for sign and scry
    
    * trailing line
    
    * typo
    
    * styling
    
    * Require Uint8Array for msg and key
    
    * compare public keys instead of addresses
    
    * make them beautiful make them shine
    
    Co-authored-by: complected <nobody>

--- coreword.js
+++ coreword.js
@@ -1,6 +1,8 @@
 import rlp from 'rlp'
 import hashes from 'js-sha3'
-import nacl from 'tweetnacl'
+import { utils as etils, Wallet } from 'ethers'
+
+const ustr = etils.toUtf8String
 
 export function roll(v) {
     return rlp.encode(v)
@@ -10,10 +12,18 @@ export function hash(b) {
     return hashes.keccak256(b)
 }
 
-export function sign(msg, key) {
-    return nacl.sign(msg, key)
+// Return EIP-191 signature as string, expect message and private key.
+// https://docs.ethers.io/v5/api/signer/#Signer-signMessage
+export async function sign(msg, key) {
+    if ( !(msg instanceof Uint8Array) || !(key instanceof Uint8Array) ) throw new Error("Message must be a Uint8Array.")
+    return (new Wallet(ustr(key))).signMessage(msg)
 }
 
-export function scry(msg, sig, key) {
-    return nacl.verify(msg, sig, key)
+// Return public key as is, only if public key is verified to have signed message. Throw an error otherwise.
+export function scry(msg, key, sig) {
+    if ( !(msg instanceof Uint8Array) || !(key instanceof Uint8Array)) throw new Error("Message and key must be Uint8Array.")
+    const digest = etils.hashMessage(ustr(msg))
+    const recovered_key = etils.recoverPublicKey(etils.arrayify(digest), sig)
+    if ( ustr(key) !== recovered_key ) throw new Error("Recovered public key and input public key do not match.")
+    return key
 }
--- test.js
+++ test.js
@@ -1,7 +1,33 @@
 import { test } from 'tapzero'
 
-import { roll } from './coreword.js'
+import { roll, scry, sign } from './coreword.js'
+
+import { utils as etils } from 'ethers'
+
+const ubyte = etils.toUtf8Bytes
+const sk_txt = "0x309a49dcda67245af724a05ff081b0c5d24ec0b87a1393d940de6c1dd60f45a3"
+const sk = ubyte(sk_txt)
+const pk_txt = etils.computePublicKey(sk_txt)
+const pk = ubyte(pk_txt)
 
 test('roll', t=>{
     t.ok(roll([]))
+})
+
+test('scry', async _=>{
+    const verify = msg=> test(
+        String.raw`TEST: ${msg}`,
+        async t=> t.equal(
+            scry(
+                ubyte(msg),
+                pk,
+                await sign(ubyte(msg), sk)
+            ),
+            pk
+        )
+    )
+    verify("What is real? That which is irreplacable.")
+    verify("")
+    verify("\\n")
+    verify(String.raw`"\\u0024"`)
 })
