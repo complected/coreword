c6313f66cc007181b101bf74f19371b0ef026203
complected <nobody>
2022-05-31 12:59:18 -0700

    test.js styling

--- test.js
+++ test.js
@@ -1,33 +1,21 @@
-import { test } from 'tapzero'
+import { test } from 'tapzero';
 
-import { roll, scry, sign } from './coreword.js'
+import { scry, sign, ubye, pkey } from './coreword.js';
 
-import { utils as etils } from 'ethers'
+const sk = ubye("0x309a49dcda67245af724a05ff081b0c5d24ec0b87a1393d940de6c1dd60f45a3");
+const pk = pkey(sk);
 
-const ubyte = etils.toUtf8Bytes
-const sk_txt = "0x309a49dcda67245af724a05ff081b0c5d24ec0b87a1393d940de6c1dd60f45a3"
-const sk = ubyte(sk_txt)
-const pk_txt = etils.computePublicKey(sk_txt)
-const pk = ubyte(pk_txt)
-
-test('roll', t=>{
-    t.ok(roll([]))
-})
-
-test('scry', async _=>{
-    const verify = msg=> test(
-        String.raw`TEST: ${msg}`,
-        async t=> t.equal(
-            scry(
-                ubyte(msg),
-                pk,
-                await sign(ubyte(msg), sk)
-            ),
-            pk
-        )
-    )
-    verify("What is real? That which is irreplacable.")
-    verify("")
-    verify("\\n")
-    verify(String.raw`"\\u0024"`)
-})
+test('scry', async _ => {
+  const verify = msg_str => test(
+    String.raw`${msg_str}:`,
+    async t => {
+      const msg = ubye(msg_str);
+      const sig = await sign(msg, sk);
+      t.equal(scry(msg, pk, sig), pk);
+    }
+  );
+  verify("What is real? That which is irreplacable.");
+  verify("");
+  verify("\\n");
+  verify(String.raw`"\\u0024"`);
+});
