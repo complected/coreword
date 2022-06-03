bd437819c8399f707575508a4d90c4daa0cc9a8f
nikolai <mail@nikolai.fyi>
2022-06-03 15:11:34 -0400

    clean

--- core/word.ts
+++ core/word.ts
@@ -2,43 +2,53 @@ import rlp from 'rlp'
 import hashes from 'js-sha3'
 import * as secp from "@noble/secp256k1"
 
+export {
+    Hexs, Blob,
+    Roll, Hash,
+    Pubk, Seck, Sign,
+    blob, roll, hash,
+    sign, scry,
+    Okay, fail, toss
+}
 
 type Blob = Buffer
 type Roll = Blob | Roll[]
-type Hash = Blob // 32 bytes
-type Pubk = Blob // 33 bytes. test.js [8]
-type Seck = Blob // 32 bytes. test.js [9]
-type Sign = Blob // 65 bytes. test.js [7]
+type Hash = Blob   // 32 bytes
+type Pubk = Blob   // 33 bytes
+type Seck = Blob   // 32 bytes
+type Sign = Blob   // 65 bytes
 type Hexs = string // hex string
 
-
-export function blob(hex : Hexs) : Blob {
+function blob(hex : Hexs) : Blob {
     return Buffer.from(hex, 'hex')
 }
 
-export function roll(r : Roll) : Blob {
+function roll(r : Roll) : Blob {
     return Buffer.from(rlp.encode(r))
 }
 
-export function hash(b : Blob) : Hash {
+function hash(b : Blob) : Hash {
     return Buffer.from(hashes.keccak256(b), 'hex')
 }
 
 // Return r, s, and v concatenated.
-export async function sign(msg: Blob, key: Seck, opts: any = { fake: false }): Promise<Sign> {
-    if (opts.fake === true) {
-        return Buffer.from('fakes'.repeat(13)) // 65 bytes
-    } else {
-        const [sig, recovery] = await secp.sign(msg, key, { recovered: true, der: false })
-        return Buffer.concat([sig, Buffer.from([recovery])])
-    }
+async function sign(msg: Blob, key: Seck, opts: any = { fake: false }): Promise<Sign> {
+    const [sig, recovery] = await secp.sign(msg, key, { recovered: true, der: false })
+    return Buffer.concat([sig, Buffer.from([recovery])])
 }
 
 // Return (compressed) public key.
-export function scry(msg : Blob, sig : Sign, opts : any = {fake:false}) : Pubk {
-    if (opts.fake === true) {
-        return Buffer.concat([Buffer.from([0x0]), Buffer.from('pubk'.repeat(8))]) // 33 bytes
-    } else {
-	    return Buffer.from(secp.recoverPublicKey(msg, sig.slice(0, 64), sig[64], true))
-    }
+function scry(msg : Blob, sig : Sign, opts : any = {fake:false}) : Pubk {
+    return Buffer.from(secp.recoverPublicKey(msg, sig.slice(0, 64), sig[64], true))
+}
+
+
+type Okay<T> = T | Error
+
+function fail(desc:string) : Error {
+    return new Error(desc)
+}
+
+function toss(desc:string) {
+    throw new Error(desc)
 }
